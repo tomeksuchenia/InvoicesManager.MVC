942ba6e86c31971224018616f18b036cec941ab2
tomeksuchenia <tomeksuchenia@gmail.com>
2023-11-25 16:48:33 +0100

    Added Controller & View

---
+++ InvoicesManagerWebApp/Controllers/InvoicesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoicesManagerWebApp.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
